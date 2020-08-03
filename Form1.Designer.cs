68d7fda94810af44249cbdeecef123f6bb622025
Vic Graham <vic.graham@ntlworld.com>
2020-08-03 20:04:41 +0100

    Initial load to repository

---
+++ Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace FlightSchedule
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnReadFile = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.gridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnReadFile
+            // 
+            this.btnReadFile.Location = new System.Drawing.Point(336, 37);
+            this.btnReadFile.Name = "btnReadFile";
+            this.btnReadFile.Size = new System.Drawing.Size(218, 51);
+            this.btnReadFile.TabIndex = 0;
+            this.btnReadFile.Text = "Open and Read File";
+            this.btnReadFile.UseVisualStyleBackColor = true;
+            this.btnReadFile.Click += new System.EventHandler(this.BtnReadFile_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            this.folderBrowserDialog1.SelectedPath = "C:\\Users\\vicgr\\source\\repos\\FlightSchedule";
+            // 
+            // gridView
+            // 
+            this.gridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridView.Location = new System.Drawing.Point(32, 129);
+            this.gridView.Name = "gridView";
+            this.gridView.RowHeadersWidth = 62;
+            this.gridView.RowTemplate.Height = 28;
+            this.gridView.Size = new System.Drawing.Size(824, 292);
+            this.gridView.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkBlue;
+            this.ClientSize = new System.Drawing.Size(889, 450);
+            this.Controls.Add(this.gridView);
+            this.Controls.Add(this.btnReadFile);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Form1";
+            this.Text = "Create PFPX schedule";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReadFile;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.DataGridView gridView;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Text;
+using OfficeOpenXml;
+
+namespace FlightSchedule
+{
+    public partial class Form1 : Form
+    {
+        private string fName = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnReadFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fdlg = new OpenFileDialog
+            {
+                Title = "Schedule File Dialog",
+                InitialDirectory = @"C:\Users\vicgr\source\repos\FlightSchedule",
+                Filter = "All files (*.*)|*.*|Excel files (*.*)|*.xlsm",
+                FilterIndex = 2,
+                RestoreDirectory = true
+            };
+            if (fdlg.ShowDialog() == DialogResult.OK)
+            {
+                // Open file for reading
+                fName = fdlg.FileName;
+                if (!string.IsNullOrEmpty(fName))
+                {
+                    OpenAndReadFile(fdlg.FileName);
+                }
+            }
+        }
+
+        private void OpenAndReadFile(string fileName)
+        {
+            //using (var fs = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+            //{
+            //    string line;
+            //    int i;
+            //    using (var sr = new StreamReader(fs, Encoding.UTF8))
+            //    {
+
+
+            //        while ((line = sr.ReadLine()) != null)
+            //        {
+            //            i = 1;
+            //        }
+
+            //    }
+            //}
+            using (var package = new ExcelPackage(new FileInfo(fileName)))
+            {
+                var firstSheet = package.Workbook.Worksheets["First Sheet"];
+                var rowCount = firstSheet.Dimension.Rows;
+                var colCount = firstSheet.Dimension.Columns;
+                gridView.RowCount = rowCount;
+                gridView.ColumnCount = colCount;
+                // Loop through each row (altername rows only)
+                for (int i = 2; i < rowCount+1; i+=2)
+                {
+
+                    string flight = firstSheet.Cells[i, 1].Text;
+                    //TODO split flight into code and number
+                    string departing = firstSheet.Cells[i, 2].Text;
+                    //TODO split departing into ICAO and departure time
+                    string duration = firstSheet.Cells[i, 4].Text;
+                    string arriving = firstSheet.Cells[i, 5].Text;
+                    //TODO split arriving onto ICAO and arrival time
+                    StringBuilder sb = new StringBuilder();
+                   
+                    for (int j = 7; j < 14; j++)
+                    {
+                        string day = firstSheet.Cells[i, j].Text;
+
+                        if (firstSheet.Cells[i, j].Text.Length > 0)
+                        {
+                            sb.Append(day);
+                        }
+                    }
+                    string daysOfOperaton = sb.ToString();
+                    string type = firstSheet.Cells[i, 15].Text;
+
+                }
+            }
+
+        }
+    }
+
+
+}
+
