49cb49c42dc1ec69327847fccc4811a4ce4aa1a9
Vic Graham <vic.graham@ntlworld.com>
2020-08-22 22:35:04 +0100

    Added class library reference

--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,8 @@ using System.IO;
 using System.Windows.Forms;
 using System.Text;
 using OfficeOpenXml;
+using FlightInfo;
+using System.Collections.Generic;
 
 namespace FlightSchedule
 {
@@ -43,41 +45,39 @@ namespace FlightSchedule
 
         private void OpenAndReadFile(string fileName)
         {
-            //using (var fs = new FileStream(fileName, FileMode.Open, FileAccess.Read))
-            //{
-            //    string line;
-            //    int i;
-            //    using (var sr = new StreamReader(fs, Encoding.UTF8))
-            //    {
-
-
-            //        while ((line = sr.ReadLine()) != null)
-            //        {
-            //            i = 1;
-            //        }
-
-            //    }
-            //}
             using (var package = new ExcelPackage(new FileInfo(fileName)))
             {
                 var firstSheet = package.Workbook.Worksheets["First Sheet"];
                 var rowCount = firstSheet.Dimension.Rows;
                 var colCount = firstSheet.Dimension.Columns;
-                gridView.RowCount = rowCount;
-                gridView.ColumnCount = colCount;
+                int rc = 25;
+                gridView.RowCount = rc;
+                gridView.ColumnCount = 6;
+                int k = 0;
+                List<Flight> flights = new List<Flight>();
+                Flight flight;
                 // Loop through each row (altername rows only)
                 for (int i = 2; i < rowCount+1; i+=2)
                 {
-
-                    string flight = firstSheet.Cells[i, 1].Text;
+                    int l = 0;
+                    flight = new Flight();
+                    
+                    string flightNumber = firstSheet.Cells[i, 1].Text;
+                    gridView.Rows[k].Cells[l++].Value = flightNumber;
+                    flight.airline = flightNumber;
                     //TODO split flight into code and number
+                    flight.consolidatedFlightNumber = flightNumber;
                     string departing = firstSheet.Cells[i, 2].Text;
+                    gridView.Rows[k].Cells[l++].Value = departing;
+                    flight.departingFrom = departing;
                     //TODO split departing into ICAO and departure time
                     string duration = firstSheet.Cells[i, 4].Text;
+                    gridView.Rows[k].Cells[l++].Value = duration;
                     string arriving = firstSheet.Cells[i, 5].Text;
+                    gridView.Rows[k].Cells[l++].Value = arriving;
+                    flight.arrivingAt = arriving;
                     //TODO split arriving onto ICAO and arrival time
-                    StringBuilder sb = new StringBuilder();
-                   
+                    StringBuilder sb = new StringBuilder();                   
                     for (int j = 7; j < 14; j++)
                     {
                         string day = firstSheet.Cells[i, j].Text;
@@ -88,7 +88,15 @@ namespace FlightSchedule
                         }
                     }
                     string daysOfOperaton = sb.ToString();
+                    gridView.Rows[k].Cells[l++].Value = daysOfOperaton;
                     string type = firstSheet.Cells[i, 15].Text;
+                    gridView.Rows[k].Cells[l++].Value = type;
+                    flights.Add(flight);
+                    k++;
+                    if(k >= rc)
+                    {
+                        break;
+                    }
 
                 }
             }
